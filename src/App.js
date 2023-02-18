7ee318fa27cc91add9090eb1cf28295b5f27849b
David Etuk <djetuk@gmail.com>
2023-02-18 23:03:48 +0100

    fix: removing unneccessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./components/sign-in/signin.component";
+import SignUp from "./components/signup/signup.component";
+import Blog from "./components/blog-main/blog.component";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Blog />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
