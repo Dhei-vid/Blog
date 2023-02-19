c8d00190d9822f701a243337171abe6acdb28ecb
David Etuk <djetuk@gmail.com>
2023-02-19 17:19:53 +0100

    feat: set up the respective categories pages

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
+    letterSpacing: {
+      wider: ".40rem",
+      widest: ".85rem",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        a: "-4.16rem",
+        x: "2.40rem",
+        82: "21.6rem",
+        84: "22.5rem",
+        86: "23.4rem",
+        100: "25rem",
+        110: "30rem",
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+};
