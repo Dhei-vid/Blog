2cf87ff87c4c26887ebf755f5416ad02bc12910d
David Etuk <djetuk@gmail.com>
2023-02-19 17:15:09 +0100

    feact: linked navigation items to their respective pages using route and outlet

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,40 +1,52 @@
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
 const categories = [
   {
     title: "Entertainment",
-    link: "",
+    link: "entertainment",
   },
   {
     title: "Food",
-    link: "",
+    link: "food",
   },
   {
     title: "Travel",
-    link: "",
+    link: "travel",
   },
   {
     title: "Sport",
-    link: "",
+    link: "sport",
   },
 ];
 
 const Navigation = () => {
   return (
-    <div className="flex justify-center space-x-60 my-10">
-      <span className="uppercase  cursor-pointer">Blog</span>
-      <ul className="list-none flex gap-x-12">
-        {categories.map((category, index) => (
-          <li
-            className="cursor-pointer after:content-[*] hover:border-b-2 border-teal-500 scale-x-100 ease-in-out delay-100"
-            key={index}
-          >
-            {category.title}
-          </li>
-        ))}
-      </ul>
-      <div className="flex gap-x-10 uppercase">
-        <div className="cursor-pointer"> Log in</div>
-        <div className="cursor-pointer">Create an Account</div>
+    <div>
+      <div className="flex justify-center space-x-60 my-10">
+        <span className="uppercase  cursor-pointer">
+          <Link to="/">Blog</Link>
+        </span>
+        <ul className="list-none flex gap-x-12">
+          {categories.map((category, index) => (
+            <li
+              className="cursor-pointer after:content-[*] hover:border-b-2 border-teal-500 scale-x-100 ease-in-out delay-100"
+              key={index}
+            >
+              <Link to={category.link}>{category.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <div className="flex gap-x-10 uppercase">
+          <div className="cursor-pointer border-2 rounded-md border-teal-300 border-spacing-1">
+            <Link to="/login">Log in</Link>
+          </div>
+          <div className="cursor-pointer">
+            <Link to="/signup">Create an Account</Link>
+          </div>
+        </div>
       </div>
+      <Outlet />
     </div>
   );
 };
