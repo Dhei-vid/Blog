93effd2f99936c2aae4e66783924497e82dd2636
David Etuk <djetuk@gmail.com>
2023-02-19 17:18:41 +0100

    feat: imported respective components and linked them

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 
+import Home from "./components/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./components/sign-in/signin.component";
+import Food from "./components/categories/food/food.component";
+import Travel from "./components/categories/travel/travel.component";
+import Entertainment from "./components/categories/entertainment/entertainment.component";
+import Sport from "./components/categories/sport/sport.component";
+import LogIn from "./components/login/login.component";
 import SignUp from "./components/signup/signup.component";
-import Blog from "./components/blog-main/blog.component";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route index element={<Blog />} />
-        <Route path="signin" element={<SignIn />} />
+        <Route index element={<Home />} />
+        <Route path="entertainment" element={<Entertainment />} />
+        <Route path="travel" element={<Travel />} />
+        <Route path="sport" element={<Sport />} />
+        <Route path="food" element={<Food />} />
+        <Route path="login" element={<LogIn />} />
         <Route path="signup" element={<SignUp />} />
       </Route>
     </Routes>
