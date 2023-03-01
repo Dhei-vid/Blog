1d378e14e1841cb379230ca20be4e2bd897fef02
David Etuk <djetuk@gmail.com>
2023-03-01 23:37:36 +0100

    feat: validated form with Yup and formik react hook

--- src/components/signup/signup.component.jsx
+++ src/components/signup/signup.component.jsx
@@ -3,6 +3,26 @@ import * as Yup from "yup";
 
 import Button from "../button/button.component";
 
+const SignUpSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email!").required("Email is Required"),
+  password: Yup.string()
+    .min(8, "Password is too short - should be 8 chars minimum.")
+    .matches(/[a-zA-Z]/, "Password can only contain Latin letters.")
+    .required("Password is Required"),
+  confirmpassword: Yup.string().when("password", {
+    is: (val) => (val && val.length ? true : false),
+    then: Yup.string().oneOf([Yup.ref("password")], "Password does not match"),
+  }),
+});
+
 const SignUp = () => {
   const formik = useFormik({
     initialValues: {
@@ -10,12 +30,16 @@ const SignUp = () => {
       lastName: "",
       email: "",
       password: "",
+      confirmpassword: "",
     },
     validateOnBlur: true,
+    validationSchema: { SignUpSchema },
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
+
+  console.log("Errors: ", formik.errors);
 
   return (
     <div className="m-16">
@@ -59,6 +83,16 @@ const SignUp = () => {
           type="password"
           onChange={formik.handleChange}
           value={formik.values.password}
+        />
+
+        <label htmlFor="password"> Confirm Password </label>
+        <input
+          id="confirmpassword"
+          className="shadow appearance-none border border-blue-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          name="confirmpassword"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.confirmpassword}
         />
         <Button buttonName={`Sign Up`} />
       </form>
