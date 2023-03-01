b211ff6e70f9b9ece986d7006f1749dd337111de
David Etuk <djetuk@gmail.com>
2023-03-01 23:36:33 +0100

    feat: added a  button component for default buttons

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,14 @@
+const Button = ({ buttonName }) => {
+  return (
+    <div>
+      <button
+        type="submit"
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        {buttonName}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
